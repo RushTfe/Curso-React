ca2787ee72932a5ae76494634c21c66a778f5f70
RushTfe <pedrogalindotrabajo@hotmail.com>
2022-03-11 08:28:29 +0000

    ultimos cambios

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,50 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import { GifGridItem } from './GifGridItem';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from '../components/GifGridItem'
 
 export const GifGrid = ({ category }) => {
 
+	const { data:images, loading } = useFetchGifs( category );
 
-  const [images, setImages] = useState([]);
+  	return (
+		<>
+			<h3 className='animate__animated animate__fadeIn'>{ category }</h3>
 
-  useEffect( () => getGifs(), []);
+			{ loading && <p className='animate__animated animate__flash'>Loading</p> }
 
-
-    const getGifs = async() => {
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=onepunch&limit=10&api_key=7mmZNL56QHm56pSMJqhgxSDWNsXia1B1'
-        const resp = await fetch ( url );
-
-        const { data } = await resp.json();
-        
-        const gifs = data.map(  img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        });
-
-        console.log(gifs);
-
-        setImages( gifs );
-    
-    }
-
-  return (
-	  <>
-		<h3>{ category }</h3>
-		<div className='card-grid'>
-
-			{
-				images.map( img => (
-					<GifGridItem  
-						key={ img.id }
-						{ ...img } 
-					/>
-				))
-			}
-		</div>
-	</>
+			<div className='card-grid'>
+				{
+					images.map( img => <GifGridItem key={ img.id }{ ...img } />)
+				}
+			</div>
+		</>
 
   )
 }
